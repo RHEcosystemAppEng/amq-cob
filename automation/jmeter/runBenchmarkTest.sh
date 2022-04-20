275d3fc62e7c2d0168362461a5a571fd20bcbe5d
Lokesh Rangineni <lokeshforjava@gmail.com>
2022-04-20 12:03:47 -0400

    Added properties for producer sample size and consumer sample size. setting JVM args before executing jmeter CLI.

--- automation/jmeter/runBenchmarkTest.sh
+++ automation/jmeter/runBenchmarkTest.sh
@@ -2,15 +2,18 @@
 
 function runBenchmark(){
 local HOST_ADDRESS=$1
-printf "Running jmeter test and generating JTL file."
 
+printf "Running jmeter test and generating JTL file."
+JVM_ARGS="-Xms4g -Xmx6g"
 jmeter -n -t "${currentDir}/AMQ_Jmeter_Test_Plan_local.jmx" \
   -JproducerDuration=10 \
   -JconsumerDuration=20 \
+  -JproducerSamples=1000 \
+  -JconsumerSamples=2000 \
   -l "${outputDir}/jmeter_results_${HOST_ADDRESS}.jtl"
 
 printf  "Calculating throughput from JTL file."
-./gen_throughput.sh "-i" "${outputDir}/jmeter_results_${HOST_ADDRESS}.jtl" -a 200 -b 200 -f csv -c "JMS Subscriber" -p "JMS Publisher" > "${outputDir}/jmeter_csv_results_${HOST_ADDRESS}.csv"
+./gen_throughput.sh "-i" "${outputDir}/jmeter_results_${HOST_ADDRESS}.jtl" -a 1000 -b 2000 -f csv -c "JMS Subscriber" -p "JMS Publisher" > "${outputDir}/jmeter_csv_results_${HOST_ADDRESS}.csv"
 
 printf  "Benchmark is done..!!"
 }
@@ -24,7 +27,6 @@ function generateJndiPropertiesFile(){
 
 BROKER_IP_ADDRESS=$1
 ROUTER_IP_ADDRESS=$2
-JVM_ARGS="-Xms4048m -Xmx6096m"
 
 currentDir=$(pwd)
 
@@ -36,5 +38,5 @@ mkdir -p "${outputDir}"
 generateJndiPropertiesFile "${BROKER_IP_ADDRESS}"
 runBenchmark "${BROKER_IP_ADDRESS}"
 
-#generateJndiPropertiesFile "${ROUTER_IP_ADDRESS}"
-#runBenchmark "${ROUTER_IP_ADDRESS}"
+generateJndiPropertiesFile "${ROUTER_IP_ADDRESS}"
+runBenchmark "${ROUTER_IP_ADDRESS}"
