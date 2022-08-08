cbb4d0dadd6aab25de2bef05f3ff3b59288b558b
Sandip Gahlot <sgahlot@redhat.com>
2022-08-08 14:02:14 -0400

    APPENG-916: Refactored inter-region Terraform config for AWS. Also added "is defined" for enable_ssl otherwise it was throwing an error when this variable was not defined.

--- automation/terraform/multi-region/locals.tf
+++ automation/terraform/multi-region/locals.tf
@@ -5,8 +5,8 @@ locals {
   R2_VPC2_MAIN_CIDR_BLOCK = "${var.R2_VPC2_PRIVATE_IP_PREFIX}.0.0/16"
 
   NAME_PREFIX = "${var.PREFIX}-AMQ_COB"
-  R1_AMI_ID      = lookup(var.AMI, var.REGION_1)
-  R2_AMI_ID      = lookup(var.AMI, var.REGION_2)
+  R1_AMI_ID   = lookup(var.AMI, var.REGION_1)
+  R2_AMI_ID   = lookup(var.AMI, var.REGION_2)
 
   KEY_NFS       = "nfs"
   KEY_BROKER_01 = "broker_01"
---
+++ automation/terraform/vpc-peering/inter-region/output.tf
@@ -0,0 +1,3 @@
+output "peering-info" {
+  value = aws_vpc_peering_connection.r1-r2-vpc
+}
